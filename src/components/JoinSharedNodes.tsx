22a56bff77ee9c076939c490a1dc2e04a55b76dd
Phoenix Dev🐦 <55408180+phoenixdev0117@users.noreply.github.com>
2025-04-12 01:45:21 -0700

    feat: add linear gradient color

--- src/components/JoinSharedNodes.tsx
+++ src/components/JoinSharedNodes.tsx
@@ -4,18 +4,18 @@ export default function JoinSharedNodes() {
         <main className="pt-[20px] pb-[30px]">
             <div className="flex flex-col justify-center items-center text-center">
                 <div className="text-[30px] font-family-sora font-bold pb-[30px]">JOIN A SHARED NODE</div>
-                <div className="p-[1px] frame-border rounded-[32px]">
+                <div className=" frame-border rounded-[32px]">
                     <div className="w-full h-full frame-body rounded-[32px] px-[30px] md:px-[105px]">
                         <h1 className="text-[30px] font-family-sora font-semibold text-center pt-[35px] pb-[20px]">Choose Hosting Plan</h1>
                         <div className="flex flex-col sm:flex-row justify-between items-center gap-[17px] pb-[35px]">
-                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[1px] bg-gradient-to-r from-[#00AEB9] to-[#EC008C] rounded-[34px]">
-                                <button className="w-full h-full bg-[#252525] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">1 Month</button>
+                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[2px] linearGradient rounded-[34px]">
+                                <button className="w-full h-full bg-[#0c1131] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">1 Month</button>
                             </div>
-                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[1px] bg-gradient-to-r from-[#00AEB9] to-[#EC008C] rounded-[34px]">
-                                <button className="w-full h-full bg-[#252525] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">3 Months</button>
+                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[2px] linearGradient rounded-[34px]">
+                                <button className="w-full h-full bg-[#0c1131] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">3 Months</button>
                             </div>
-                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[1px] bg-gradient-to-r from-[#00AEB9] to-[#EC008C] rounded-[34px]">
-                                <button className="w-full h-full bg-[#252525] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">6 Months</button>
+                            <div className="inline-block w-full sm:w-[147px] h-[40px] p-[2px] linearGradient rounded-[34px]">
+                                <button className="w-full h-full bg-[#0c1131] text-white font-family-sora font-semibold text-[18px] rounded-[34px] cursor-pointer">6 Months</button>
                             </div>
                         </div>
                     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar() {
           <Link href="/privatenodes" className="hover:text-blue-400 font-family-sora text-xl text-[#B0B0B0] transition-colors duration-400">PRIVATE NODES</Link>
           <Link href="/dashboard" className="hover:text-blue-400 font-family-sora text-xl text-[#B0B0B0] transition-colors duration-400">DASHBOARD</Link>
         </div>
-        <div className="flex justify-between items-center space-x-4">
+        <div className="flex justify-between items-center gap-[23px]">
           <div className="relative xl:hidden">
             <button ref={menuButtonRef} onClick={() => setMenuOpen(!menuOpen)} className="text-white focus:outline-none">
               {menuOpen ? <XIcon className="h-6 w-6 cursor-pointer" /> : <MenuIcon className="h-6 w-6 cursor-pointer" />}
@@ -69,54 +69,54 @@ export default function Navbar() {
           <div className="hidden sm:flex items-center">
             <Image src="/navbar/notification.svg" alt="Notification" width={50} height={50} className="w-11 h-10" />
           </div>
-          <div className="hidden md:flex items-center space-x-2 sm:space-x-4">
+          <div className="hidden md:flex items-center gap-[8px]">
             <Link href="/login">
-              <div className="inline-block p-[1px] bg-gradient-to-r from-[#1ec0ca] to-[#ec008c] rounded-full">
-                <button className="h-9 w-24 px-4 py-2 font-family-sora text-white text-sm bg-[#080525] rounded-full cursor-pointer">
+              <div className="inline-block linearGradient rounded-full w-[94px] h-[36px]" style={{"padding": "1.6px" }}>
+                <button className="h-full w-full px-4 py-2 font-family-sora text-white text-[11px] bg-[#080525] rounded-full cursor-pointer">
                   LOG IN
                 </button>
               </div>
             </Link>
             <Link href="/signup">
-              <div className="inline-block p-[1px] bg-gradient-to-r from-[#1ec0ca] to-[#ec008c] rounded-full">
-                <button className="h-9 w-24 px-4 py-2 font-family-sora text-white text-sm bg-[#080525] rounded-full cursor-pointer">
+              <div className="inline-block linearGradient rounded-full w-[94px] h-[36px]" style={{"padding": "1.6px" }}>
+                <button className="h-full w-full px-4 py-2 font-family-sora text-white text-[11px] bg-[#080525] rounded-full cursor-pointer">
                   SIGN UP
                 </button>
               </div>
             </Link>
-            <div className="relative">
-              <button ref={accountButtonRef} onClick={() => setAccountOpen(!accountOpen)} className='hidden md:block cursor-pointer'>
-                <Image src="/navbar/account.svg" alt="User" width={50} height={50} className='w-11 h-10' />
-              </button>
-              {accountOpen && accountButtonRef.current && (
-                <div className="absolute top-15 right-0 h-[156px] w-[151px] border-[1px] border-[#00AEB9] bg-[#272E50] rounded-[10px] 
-                flex flex-col pt-[17px] pr-[39px] pb-[12px] pl-[11px]">
-                  <h1 className='text-white text-[13px] font-family-sora font-semibold pb-[21px]'>Savvas</h1>
-                  <Link href="/myaccount" onClick={() => setAccountOpen(false)}>
-                  <div className="flex flex-row justify-start items-center gap-[9px] pb-[15px] cursor-pointer">
-                    <Image src="/myaccount/profile-icon.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
-                    <div className="text-white text-[12px] font-regular font-family-sora">My Account</div>
-                  </div>
-                  </Link>
-                  <Link href="/billing" onClick={() => setAccountOpen(false)}>
-                  <div className="flex flex-row justify-start items-center gap-[9px] pb-[15px] cursor-pointer">
-                    <Image src="/myaccount/dollar-sign.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
-                    <div className="text-white text-[12px] font-regular font-family-sora">Billing</div>
-                  </div>
-                  </Link>
-                  <div 
-                    className="flex flex-row justify-start items-center gap-[9px] cursor-pointer" 
-                    onClick={() => {
-                      setAccountOpen(false);
-                      setLogoutModalOpen(true);
-                    }}
-                  >
-                    <Image src="/myaccount/logout-icon.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
-                    <div className="text-white text-[12px] font-regular font-family-sora">Log Out</div>
-                  </div>
+          </div>
+          <div className="relative">
+            <button ref={accountButtonRef} onClick={() => setAccountOpen(!accountOpen)} className='hidden md:block cursor-pointer'>
+              <Image src="/navbar/account.svg" alt="User" width={50} height={50} className='w-11 h-10' />
+            </button>
+            {accountOpen && accountButtonRef.current && (
+              <div className="absolute top-15 right-0 h-[156px] w-[151px] border-[1px] border-[#00AEB9] bg-[#272E50] rounded-[10px] 
+              flex flex-col pt-[17px] pr-[39px] pb-[12px] pl-[11px]">
+                <h1 className='text-white text-[13px] font-family-sora font-semibold pb-[21px]'>Savvas</h1>
+                <Link href="/myaccount" onClick={() => setAccountOpen(false)}>
+                <div className="flex flex-row justify-start items-center gap-[9px] pb-[15px] cursor-pointer">
+                  <Image src="/myaccount/profile-icon.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
+                  <div className="text-white text-[12px] font-regular font-family-sora">My Account</div>
                 </div>
-              )}
-            </div>
+                </Link>
+                <Link href="/billing" onClick={() => setAccountOpen(false)}>
+                <div className="flex flex-row justify-start items-center gap-[9px] pb-[15px] cursor-pointer">
+                  <Image src="/myaccount/dollar-sign.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
+                  <div className="text-white text-[12px] font-regular font-family-sora">Billing</div>
+                </div>
+                </Link>
+                <div 
+                  className="flex flex-row justify-start items-center gap-[9px] cursor-pointer" 
+                  onClick={() => {
+                    setAccountOpen(false);
+                    setLogoutModalOpen(true);
+                  }}
+                >
+                  <Image src="/myaccount/logout-icon.svg" alt="User" width={50} height={50} className='w-[18px] h-[18px]' />
+                  <div className="text-white text-[12px] font-regular font-family-sora">Log Out</div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
